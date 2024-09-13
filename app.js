a397cd60fc30416b326a57c48c40efd4a9697a7e
Rafay Ahmed <154263565+Rafay-Ahmed178@users.noreply.github.com>
2024-09-14 04:33:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const arr = [
+      [5, 4], 
+      [9, 2, 10,9, 2], 
+       [7, 15, 7], 
+       [7], 
+     ];
+    
+ for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].length);
+    
+       for (let j = 0; j < arr[i].length; j++) {
+         console.log(arr[i]  [j]);
+       }
+     }
